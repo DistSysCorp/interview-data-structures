ff6f82c87a990d677e9dcd3403fcd30605edc77e
qtmuniao <songpengwei40@gmail.com>
2024-03-30 14:50:01 +0800

    Update LRUCache.cpp

--- LRUCache.cpp
+++ LRUCache.cpp
@@ -1,22 +1,3 @@
-/*
- * Design a data structure that follows the constraints of a Least Recently Used (LRU) cache.
-
-Implement the LRUCache class:
-
-LRUCache(int capacity) Initialize the LRU cache with positive size capacity.
-Implement 2 APIs,
-
-int get(int key)
-Return the value of the key if the key exists, otherwise return -1.
-
-
-void put(int key, int value)
-Update the value of the key if the key exists. Otherwise, add the key-value pair to the cache. If the number of keys exceeds the capacity from this operation, evict the least recently used key.
-
-
-The functions get and put must each run in O(1) average time complexity.
-*/
-
 #include <iostream>
 #include <sstream>
 #include <unordered_map>
